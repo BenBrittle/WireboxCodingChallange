aea0483223922768fbe7e8ee544ac0abce663e12
themadmonkeyman <benbrittle@icloud.com>
2023-10-05 19:45:16 +0100

    ?

--- app.py
+++ app.py
@@ -56,8 +56,8 @@ def delete(id):
     try:
         db.session.delete(Question_to_delete)
         db.session.commit()
-        #return redirect('/viewQuestions')
-        return str(id)
+        return redirect('/viewQuestions')
+
     except:
         return 'there was a problem deleting that question'
 
