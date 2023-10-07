b8d999faf50b1016595ba99bea5df0258f8e88fd
themadmonkeyman <benbrittle@icloud.com>
2023-10-07 08:53:18 +0100

    test

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
-current_Question = ''
+
 
 class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -24,6 +24,7 @@ def index():
 
 @app.route('/question', methods=['POST','GET'])
 def question():
+    current_Question = ''
     if request.method == 'POST':
         submitted_answer = request.form['answer']
         if submitted_answer == current_Question.Correct_Answer:
@@ -36,7 +37,7 @@ def question():
         
         questionvar = Question.query.get(random.choices(randomQuestion))
         current_Question = questionvar
-        return render_template('Question.html', Question = questionvar)
+        return render_template('Question.html', Question = random.choices(AllQuestion))
 
 
 @app.route('/addQuestion', methods=['POST','GET'])
